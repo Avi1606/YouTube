dff95215cb2692c1176cb344972ea068e9fca2bb
Avikumar <aviiiprajapati16@gmail.com>
2025-05-07 23:41:14 +0530

    feat: conditionally render Sidebar based on Redux store state

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 
 const Sidebar = () => {
+    const showSidebar = useSelector(store => store.app.showMenu);
+
     return (
-        <div className="shadow-lg w-48 p-3 ">
-            <ul className="p-3">
-                <li> Home</li>
-                <li> Shorts</li>
-                <li> Video</li>
-                <li> Live</li>
-            </ul>
-            <h1 className="font-bold">Subscriptions</h1>
-            <ul className="p-3">
-                <li> Home</li>
-                <li> Shorts</li>
-                <li> Gaming</li>
-                <li> Movies</li>
-            </ul>
-            <h1 className="font-bold">Watch Later</h1>
-            <ul className="p-3">
-                <li> Home</li>
-                <li> Shorts</li>
-                <li> Gaming</li>
-                <li> Movies</li>
-            </ul>
+        <div>
+            {showSidebar &&
+                <div className="shadow-lg w-48 p-3 ">
+                    <ul className="p-3">
+                        <li> Home</li>
+                        <li> Shorts</li>
+                        <li> Video</li>
+                        <li> Live</li>
+                    </ul>
+                    <h1 className="font-bold">Subscriptions</h1>
+                    <ul className="p-3">
+                        <li> Home</li>
+                        <li> Shorts</li>
+                        <li> Gaming</li>
+                        <li> Movies</li>
+                    </ul>
+                    <h1 className="font-bold">Watch Later</h1>
+                    <ul className="p-3">
+                        <li> Home</li>
+                        <li> Shorts</li>
+                        <li> Gaming</li>
+                        <li> Movies</li>
+                    </ul>
+                </div>}
         </div>
     );
 };
