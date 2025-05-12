16d4ec924fbf331565128af973c287a8d191d4c4
Avikumar <aviiiprajapati16@gmail.com>
2025-05-13 00:35:12 +0530

    feat: implement video fetching in VideoContainer component

--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {YOUTUBE_API} from "../utils/constants.jsx";
 
 const VideoContainer = () => {
+
+    useEffect(() => {
+        getVideos();
+    },[]);
+
+    const getVideos = async () => {
+        const data = await fetch(YOUTUBE_API);
+        const json = await data.json();
+        console.log(json);
+    };
+
     return (
         <div>
-            <p></p>
+            <p>video container</p>
         </div>
     );
 };
---
+++ src/utils/constants.jsx
@@ -0,0 +1,14 @@
+export const HAMBURGER_ICON = "https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-icon-download-in-svg-png-gif-file-formats--crispy-user-interface-pack-icons-462145.png";
+
+export const YOUTUBE_ICON = "https://www.logo.wine/a/logo/YouTube/YouTube-Logo.wine.svg";
+
+export const USER_ICON = "https://cdn-icons-png.flaticon.com/512/666/666201.png";
+
+const YOUTUBE_API_KEY = "AIzaSyChrXYlprRt5GcK02mjuGYrbm6icywaJr8";
+
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="
+    + YOUTUBE_API_KEY;
+
+
+
+
